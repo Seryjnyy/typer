81cde3d72626c7d13323b6fb158d5797ace4b7d8
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-07-25 23:43:21 +0100

    Added the main functionality
    - Enabled user to type.
    
    - Enabled user to see if they typed in the correct character.
    
    - Enabled user to see their progress.
    
    - Enabled user to select content to go through.

--- src/text-display.tsx
+++ src/text-display.tsx
@@ -1,51 +1,134 @@
-import { useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import { useHotkeys } from "react-hotkeys-hook";
-import { textContent } from "./content";
+import { songs } from "./content";
 
 export default function TextDisplay() {
-  const [text, setText] = useState(textContent);
+  const [songIndex, setSongIndex] = useState(0);
+  const song = useMemo(() => songs[songIndex], [songIndex, songs]);
 
   const [userInput, setUserInput] = useState<string>("");
-  const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
 
   const focus = () => {
     inputRef.current?.focus();
   };
 
   useHotkeys("*", (k, handler) => {
-    //   if (k.key.length > 1 || k.key.length == 0) return;
-    //   console.log(k.key);
     focus();
   });
 
+  const res = useMemo(() => {
+    let accuracy = 0;
+    let inputted = 0;
+    let correct = 0;
+    let wrong = 0;
+
+    const display = Array.from(song.content).map((ch, i) => {
+      if (i == userInput.length) {
+        inputted = i;
+        return (
+          <span className="bg-yellow-200 p-1 rounded-md" key={i}>
+            {ch}
+          </span>
+        );
+      }
+
+      if (i > userInput.length)
+        return (
+          <span key={i} className="rounded-md">
+            {ch}
+          </span>
+        );
+
+      if (ch == userInput.charAt(i)) {
+        correct += 1;
+        return (
+          <span className="bg-green-200 " key={i}>
+            {ch}
+          </span>
+        );
+      } else {
+        wrong += 1;
+        return (
+          <span className="bg-red-200 " key={i}>
+            {ch}
+          </span>
+        );
+      }
+
+      return ch;
+    });
+
+    // if (userInput.length === 0) {
+    //   console.log(100);
+    // } else {
+    //   accuracy = ((correct - wrong) / userInput.length) * 100;
+    //   console.log(accuracy);
+    // }
+
+    return {
+      display: display,
+      // accuracy: 100,
+      totalChar: song.content.length,
+      inputChar: userInput.length,
+      // :
+    };
+  }, [song.content, userInput]);
+
+  const onNextContent = () => {
+    setSongIndex((prev) => {
+      console.log(songs.length, prev + 1);
+      if (prev + 1 >= songs.length) return prev;
+      return prev + 1;
+    });
+  };
+
+  const onPrevContent = () => {
+    setSongIndex((prev) => {
+      if (prev - 1 < 0) return prev;
+
+      return prev - 1;
+    });
+  };
+
   return (
     <div className="px-24 p-24">
-      {userInput}
-      <div className="w-fit h-fit relative">
-        <input
+      {/* {userInput} */}
+      <div className="w-full h-full bg-red-200 relative">
+        <textarea
           value={userInput}
           onChange={(e) => setUserInput(e.target.value)}
           placeholder="what"
-          className="w-full h-full border opacity-0"
+          className=" border opacity-0 absolute"
           ref={inputRef}
         />
-        <div className=" whitespace-pre-wrap ">
-          {Array.from(text).map((ch, i) => {
-            if (i == userInput.length)
-              return <span className="bg-yellow-200">{ch}</span>;
 
-            if (i >= userInput.length) return ch;
+        <div className="border p-4  border-black space-y-2">
+          <div className="border p-4 border-black">
+            <div>{`${res.inputChar}/${res.totalChar}`}</div>
+            <div>{`${song.source} - ${song.title}`}</div>
+          </div>
 
-            if (ch == userInput.charAt(i)) {
-              return <span className="bg-green-200">{ch}</span>;
-            } else {
-              return <span className="bg-red-200">{ch}</span>;
-            }
-
-            return ch;
-          })}
+          <div className="space-x-2">
+            <button
+              onClick={onPrevContent}
+              className="border px-8 py-2 border-black"
+              disabled={songIndex - 1 < 0}
+            >
+              prev
+            </button>
+            <button
+              onClick={onNextContent}
+              className="border px-8 py-2 border-black"
+              disabled={songIndex + 1 >= songs.length}
+            >
+              next
+            </button>
+          </div>
+          <div className=" whitespace-pre-wrap bg-blue-200 p-8 rounded-md mx-auto w-fit text-lg tracking-wide leading-10 font-semibold">
+            {res.display}
+          </div>
         </div>
-        {/* <div className=" whitespace-pre-wrap absolute">{text}</div> */}
       </div>
     </div>
   );
