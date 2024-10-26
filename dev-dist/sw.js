38af2fb7673c9057d6044e73d94ec1dcb2eda776
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-10-26 14:29:44 +0200

    Add a Spotify web api hook
    - Added a Spotify zustand store to have a singe instance of the sdk.
    
    - Added a hook to manage the Spotify sdk instance.
    
    - Added Tanstack Query for when we start fetching data from the Spotify
      api.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4eik3p2fhc8"
+    "revision": "0.c0mvf0lvsfo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
---
+++ src/spotify/use-spotify.ts
@@ -0,0 +1,68 @@
+import {
+    AuthorizationCodeWithPKCEStrategy,
+    Scopes,
+    SpotifyApi,
+} from "@spotify/web-api-ts-sdk";
+import { useEffect, useState } from "react";
+
+const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID as string;
+const redirectUrl = import.meta.env.VITE_SPOTIFY_REDIRECT_URL as string;
+const scopes = Scopes.all;
+// const apiSDKConfig  = SdkOptions
+
+// TODO : it might redirect the user to spotify auth, not sure yet, will have to test for longer
+export const useSpotify = ({ enabled = false }: { enabled?: boolean }) => {
+    const [apiSDK, setApiSDK] = useState<SpotifyApi | null>(null);
+
+    const authenticate = async () => {
+        if (apiSDK) return;
+
+        const auth = new AuthorizationCodeWithPKCEStrategy(
+            clientId,
+            redirectUrl,
+            scopes
+        );
+
+        const internalSdk = new SpotifyApi(auth, {});
+
+        try {
+            if (enabled) {
+                const { authenticated } = await internalSdk.authenticate();
+
+                if (authenticated) {
+                    console.log("Successfully set Spotify Web SDK");
+                    setApiSDK(internalSdk);
+                }
+            }
+        } catch (e: Error | unknown) {
+            const error = e as Error;
+            if (
+                error &&
+                error.message &&
+                error.message.includes("No verifier found in cache")
+            ) {
+                console.error(
+                    "If you are seeing this error in a React Development Environment it's because React calls useEffect twice. Using the Spotify SDK performs a token exchange that is only valid once, so React re-rendering this component will result in a second, failed authentication. This will not impact your production applications (or anything running outside of Strict Mode - which is designed for debugging components).",
+                    error
+                );
+            } else {
+                console.error(e);
+            }
+        }
+    };
+
+    const logout = () => {
+        apiSDK?.logOut();
+        setApiSDK(null);
+    };
+
+    useEffect(() => {
+        if (enabled) authenticate();
+    }, [clientId, redirectUrl, scopes, enabled]);
+
+    return {
+        apiSDK,
+        logout,
+        authenticate,
+    };
+};
