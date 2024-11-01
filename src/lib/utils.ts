e77e3b7c36d0d0b111e47a4a417df5a41b41e5e4
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-11-01 20:56:45 +0000

    Add playback seek bar to Spotify player
    - Added utility to convert miliseconds to human
      readable time.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -191,3 +191,12 @@ export const textModification = (
     }
     return res;
 };
+
+export const formatDurationMS = (duration_ms: number): string => {
+    const seconds = Math.floor(duration_ms / 1000) % 60;
+    const minutes = Math.floor(duration_ms / 1000 / 60);
+    return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
+        2,
+        "0"
+    )}`;
+};
---
+++ src/spotify/player/playback-seek-bar.tsx
@@ -0,0 +1,48 @@
+import { Slider } from "@/components/ui/slider";
+import { formatDurationMS } from "@/lib/utils";
+import { useState } from "react";
+import {
+    usePlaybackState,
+    useSpotifyPlayer,
+} from "react-spotify-web-playback-sdk";
+
+export default function PlaybackSeekBar() {
+    const spotifyPlayer = useSpotifyPlayer();
+    const playbackState = usePlaybackState(true, 1000);
+    const [isSeeking, setSeeking] = useState(false);
+    const [seekingPosition, setSeekingPosition] = useState(0);
+
+    const position = playbackState?.position ?? 0;
+    const duration = playbackState?.duration ?? 0;
+
+    const handleChange = (val: number) => {
+        setSeeking(true);
+        setSeekingPosition(val);
+    };
+
+    const handleChangeCommit = (val: number) => {
+        // set seeking position
+        setSeeking(false);
+        spotifyPlayer?.seek(val);
+    };
+
+    return (
+        <div className="flex gap-2">
+            <div className="text-xs text-muted-foreground pr-2">
+                {formatDurationMS(position)}
+            </div>
+
+            <Slider
+                disabled={playbackState === null}
+                value={isSeeking ? [seekingPosition] : [position]}
+                max={duration}
+                step={1}
+                onValueChange={(val) => handleChange(val[0])}
+                onValueCommit={(val) => handleChangeCommit(val[0])}
+            />
+            <div className="text-xs text-muted-foreground pl-2">
+                {formatDurationMS(duration)}
+            </div>
+        </div>
+    );
+}
