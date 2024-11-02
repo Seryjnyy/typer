77cb2fd3eac64a73fa3156b56f8d148f778b2524
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-11-02 20:12:53 +0000

    Add variants to music-playing component
    - Removed old css as styles are now in tailwind.
    
    - Added music-playing animation with bouncer keyframes to tailwind
      config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -126,7 +126,26 @@ module.exports = {
                         // top: "0",
                     },
                 },
+                bouncer: {
+                    "10%": {
+                        transform: "scaleY(0.3)" /* start by scaling to 30% */,
+                    },
+                    "30%": {
+                        transform: `scaleY(1)` /* scale up to 100% */,
+                    },
 
+                    "60%": {
+                        transform: `scaleY(0.5)` /* scale down to 50% */,
+                    },
+
+                    "80%": {
+                        transform: `scaleY(0.75)` /* scale up to 75% */,
+                    },
+
+                    "100%": {
+                        transform: `scaleY(0.6)` /* scale down to 60% */,
+                    },
+                },
                 slideDown: {
                     from: { height: "0" },
                     to: { height: "var(--radix-collapsible-content-height)" },
@@ -144,6 +163,7 @@ module.exports = {
                 playingNextSong: "playingNextSong 2000ms ease-out forwards",
                 startUp: "startUp 500ms ease-out forwards",
                 explosion: "explode 2s cubic-bezier(0,0,0.2,1) forwards",
+                musicPlaying: "bouncer 2.2s ease infinite alternate",
             },
         },
     },
