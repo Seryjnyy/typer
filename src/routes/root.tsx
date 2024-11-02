9e178a38d72a99e879ba6c51ae78702840a1b823
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-11-02 20:31:28 +0000

    Add temporary floating Spotify player for testing

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "../components/ui/toaster";
 import { TooltipProvider } from "../components/ui/tooltip";
 import Queue from "../components/queue";
 import TailwindGradientHack from "../tailwind-gradient-hack/tailwind-gradient-hack";
+import FloatingSpotifyPlayer from "@/spotify/player/floating-spotify-player";
 
 function Root() {
     return (
@@ -12,6 +13,7 @@ function Root() {
             <ThemeProvider defaultTheme="dark-rose" storageKey="vite-ui-theme">
                 <TooltipProvider delayDuration={100}>
                     <div className="h-screen overflow-hidden">
+                        <FloatingSpotifyPlayer />
                         <div className="flex flex-col h-full justify-end">
                             <main className="flex-grow w-full  flex justify-between z-0 h-[calc(100vh-4rem)]">
                                 <div className="z-30 w-full border-none sm:border sm:border-solid sm:my-2 pb-14 sm:pb-0 sm:ml-2 sm:rounded-md">
