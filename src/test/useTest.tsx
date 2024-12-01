80719a47a8d39e32bb94bf911dd241c189aef3ea
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-12-01 23:39:06 +0000

    Add hook for processing song for typer
    - Produces the various required versions of song content for typer.

--- src/test/useTest.tsx
+++ src/test/useTest.tsx
@@ -1,6 +1,9 @@
 import { useSongStore } from '@/lib/store/song-store.tsx'
 import { useQueueStore } from '@/lib/store/queue-store.tsx'
 import { useMemo } from 'react'
+import { Song } from '@/lib/types.ts'
+import { useTextModificationsStore } from '@/lib/store/text-modifications-store.tsx'
+import { splitSongIntoVerses, textModification } from '@/lib/utils.ts'
 
 const useCurrentSong = () => {
     const songList = useSongStore.use.songs()
@@ -12,3 +15,27 @@ const useCurrentSong = () => {
 }
 
 export { useCurrentSong }
+const useProcessedSongContent = (song: Song | undefined) => {
+    const txtMods = useTextModificationsStore.use.textModifications()
+
+    const songTextModified = song
+        ? textModification(song.content, txtMods)
+        : null
+
+    return useMemo(() => {
+        return song && songTextModified
+            ? {
+                  fullUnmodifiedText: song.content,
+                  fullModifiedText: songTextModified,
+                  strippedModifiedText: songTextModified.replace(
+                      /(\r\n|\n|\r)/gm,
+                      ''
+                  ),
+              }
+            : {
+                  fullModifiedText: '',
+                  fullUnmodifiedText: '',
+                  strippedModifiedText: '',
+              }
+    }, [song, songTextModified])
+}
