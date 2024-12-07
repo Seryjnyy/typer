29971d0cb9bb77e3609990cab4b016bb8b8f0c59
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-12-07 22:09:58 +0000

    Update the path / to use the new typer components

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import ErrorPage from "./routes/error-page";
-import Root from "./routes/root";
-import Appearance from "./routes/settings/appearance";
-import Storage from "./routes/settings/storage";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import "./index.css"
+import ErrorPage from "./routes/error-page"
+import Root from "./routes/root"
+import Appearance from "./routes/settings/appearance"
+import Storage from "./routes/settings/storage"
 
-import SettingsWindow from "./routes/settings/settings-window";
-import Song from "./routes/song/song-page";
-import Songs from "./routes/song/songs";
-import SongsPage from "./routes/song/songs-window";
+import SettingsWindow from "./routes/settings/settings-window"
+import Song from "./routes/song/song-page"
+import Songs from "./routes/song/songs"
+import SongsPage from "./routes/song/songs-window"
 
-import VersePage from "./routes/typer/verse/verse-page";
-import TyperPage from "./routes/typer/typer-page";
-import EditSong from "./routes/song/edit-song";
-import TestPage from "./test-page";
-import Preferences from "./routes/settings/preferences";
-import ImportExport from "./routes/settings/import-export";
-import Spotify from "./routes/settings/spotify";
-import Stats from "./routes/settings/stats";
-import PlaylistPage from "./routes/song/playlist/playlist-page";
-import PlaylistEdit from "./routes/song/playlist/playlist-edit";
-import TestPage2 from "./test-page-2";
+import VerseSourceTyper from "./components/typer/verse-source-typer.tsx"
+import EditSong from "./routes/song/edit-song"
+import Preferences from "./routes/settings/preferences"
+import ImportExport from "./routes/settings/import-export"
+import Spotify from "./routes/settings/spotify"
+import Stats from "./routes/settings/stats"
+import PlaylistPage from "./routes/song/playlist/playlist-page"
+import PlaylistEdit from "./routes/song/playlist/playlist-edit"
+import TestPage2 from "./test-page-2"
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import QueueSourceTyper from "@/components/typer/queue-source-typer.tsx"
+import TyperPage from "@/routes/typer/typer-page.tsx"
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 const router = createBrowserRouter([
     {
@@ -38,10 +38,10 @@ const router = createBrowserRouter([
         children: [
             {
                 path: "/",
-                // element: <TyperTestPage />,
-                element: <TyperPage />,
+                // element: <TyperPage />,
+                element: <QueueSourceTyper />,
             },
-            { path: "verse", element: <VersePage /> },
+            { path: "verse", element: <VerseSourceTyper /> },
             {
                 path: "/songs",
                 element: <SongsPage />,
@@ -108,7 +108,7 @@ const router = createBrowserRouter([
             },
         ],
     },
-]);
+])
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
@@ -116,4 +116,4 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
             <RouterProvider router={router} />
         </QueryClientProvider>
     </React.StrictMode>
-);
+)
