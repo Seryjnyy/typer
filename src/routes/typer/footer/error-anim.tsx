01f7b197724c97864b482ead2d051662a8a16559
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-12-07 16:51:51 +0000

    Update error animation
    - Added cross icon to it, makes it a bit more obvious.

--- src/routes/typer/footer/error-anim.tsx
+++ src/routes/typer/footer/error-anim.tsx
@@ -1,37 +1,33 @@
-import { usePreferenceStore } from "@/lib/store/preferences-store";
-import { cn } from "@/lib/utils";
-import { useEffect, useState } from "react";
+import { usePreferenceStore } from "@/lib/store/preferences-store"
+import { cn } from "@/lib/utils"
+import { useEffect, useState } from "react"
+import { X } from "lucide-react"
 
-export default function ErrorAnim({
-    errorIndex,
-}: {
-    errorIndex: number | null;
-}) {
-    const [animationKey, setAnimationKey] = useState(0);
-    const isErrorAnim = usePreferenceStore.use.isErrorAnim();
+// TODO : duplicate code with correct anim, but keeping it this way in case we want diff animations
+export default function ErrorAnim({ index }: { index: number | null }) {
+    const [animationKey, setAnimationKey] = useState(0)
+    const isErrorAnim = usePreferenceStore.use.isErrorAnim()
 
     useEffect(() => {
-        if (errorIndex != undefined) {
-            triggerAnimation();
+        if (index != 0) {
+            triggerAnimation()
         }
-    }, [errorIndex]);
+    }, [index])
 
     const triggerAnimation = () => {
-        setAnimationKey((prevKey) => prevKey + 1);
-    };
+        setAnimationKey((prevKey) => prevKey + 1)
+    }
 
-    if (!isErrorAnim) return null;
+    if (!isErrorAnim) return null
 
     return (
         <div
             key={animationKey}
-            className={cn(
-                `absolute  bg-gradient-to-tr from-red-500 rounded-full  sm:-bottom-5 bottom-10 left-0 sm:-left-5    `,
-                {
-                    "animate-explosion w-[10vw] h-[10vw] duration-300":
-                        animationKey != 0,
-                }
-            )}
-        ></div>
-    );
+            className={cn(`absolute  bg-gradient-to-tr from-red-500 rounded-full  sm:-bottom-5 bottom-10 left-0 sm:-left-5    `, {
+                "animate-explosion w-[10vw] h-[10vw] duration-300": animationKey !== 0,
+            })}
+        >
+            <X className={"size-[10vw] absolute bottom-0 left-0 text-red-500"} />
+        </div>
+    )
 }
