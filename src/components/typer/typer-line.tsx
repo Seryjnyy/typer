142b1797ff8c3f12f89e3e0ba7d6bb5a922d911b
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-12-08 17:24:56 +0000

    Refactor out the line component from flat-display
    - Refactored it out for re-use.

---
+++ src/components/typer/typer-line.tsx
@@ -0,0 +1,80 @@
+import { forwardRef, HTMLProps, memo } from "react"
+import { useTextModificationsStore } from "@/lib/store/text-modifications-store.tsx"
+import Ch, { chVariant } from "@/components/ch.tsx"
+
+interface LineBaseProps extends HTMLProps<HTMLDivElement> {
+    line: string
+    input: string
+    isCurrentLine: boolean
+    className?: string
+}
+
+const LineBase = forwardRef<HTMLDivElement, LineBaseProps>(({ line, input, isCurrentLine, className, ...props }, ref) => {
+    const difficultyModifiers = useTextModificationsStore.use.harderOptions()
+    console.log("line rerender", line)
+    return (
+        <div className={"tracking-wide"} ref={ref} {...props}>
+            {line.split("").map((ch, index) => {
+                const char = input.at(index)
+
+                let variant: chVariant
+
+                if (char === ch) {
+                    variant = "correct"
+                } else if (index >= input.length) {
+                    if (isCurrentLine) {
+                        variant = index === input.length ? "current" : "not-covered"
+                    } else {
+                        variant = "not-covered"
+                    }
+                } else {
+                    if (ch == " " || ch == "\n") {
+                        variant = "incorrect"
+                        ch = "_"
+                    } else {
+                        variant = "incorrect"
+                    }
+                }
+
+                if (variant != "correct" && variant != "incorrect") {
+                    // current, not covered
+
+                    if (isCurrentLine) {
+                        if (difficultyModifiers.cantSeeAhead) {
+                            if (ch != " " && input.length !== index) {
+                                ch = "_"
+                            }
+
+                            if (difficultyModifiers.cantSeeCurrent) {
+                                ch = "_"
+                            }
+
+                            if (difficultyModifiers.cantSeeUnderlines && input.length !== index) {
+                                variant = "normalInvisible"
+                            }
+                        }
+                    } else {
+                        if (difficultyModifiers.cantSeeAhead) {
+                            if (ch != " ") {
+                                ch = "_"
+                            }
+
+                            if (difficultyModifiers.cantSeeUnderlines) {
+                                variant = "normalInvisible"
+                            }
+                        }
+                    }
+                }
+                return (
+                    <Ch key={`char-${index}`} variant={variant} className={className}>
+                        {ch}
+                    </Ch>
+                )
+            })}
+        </div>
+    )
+})
+
+const Line = memo(LineBase)
+
+export default Line
