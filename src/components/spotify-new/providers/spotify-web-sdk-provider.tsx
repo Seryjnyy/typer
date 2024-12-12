9e225dd2aacf978203a9640dfce75ab2ff17bfc1
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-12-12 15:09:52 +0000

    Create a Spotify web SDK provider
    - Added a provider that guarantees its children an authenticated web
    SDK.
    
    - Otherwise, it provides a button to authenticate it.

---
+++ src/components/spotify-new/providers/spotify-web-sdk-provider.tsx
@@ -0,0 +1,50 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from "react"
+import { useSpotifyWebSDK } from "@/components/spotify-new/use-spotify-web-sdk.ts"
+import { Button } from "@/components/ui/button.tsx"
+import { SpotifyApi } from "@spotify/web-api-ts-sdk"
+import { Icons } from "@/components/icons.tsx"
+import LoadingMessage from "@/components/spotify-new/loading-message.tsx"
+
+const SpotifyWebSDKContext = createContext<SpotifyApi | null>(null)
+export const useSpotifyWebSDKContext = () => {
+    const context = useContext(SpotifyWebSDKContext)
+    if (!context) {
+        throw new Error("useSpotifyWebSDK must be used within a SpotifyWebSDKProvider")
+    }
+    return context
+}
+
+const SpotifyWebSDKProvider = ({ children }: { children: ReactNode }) => {
+    const { clientSDK, isSDKAuthenticated, connectSDK } = useSpotifyWebSDK()
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean | undefined>(undefined)
+
+    useEffect(() => {
+        const checkIfAuthenticated = async () => {
+            setIsAuthenticated(await isSDKAuthenticated())
+        }
+
+        checkIfAuthenticated()
+    }, [isSDKAuthenticated])
+
+    if (isAuthenticated === undefined) return <LoadingMessage />
+
+    if (isAuthenticated === false)
+        return (
+            <div>
+                <Button onClick={connectSDK} className={"flex items-center gap-2"} variant={"secondary"}>
+                    <Icons.spotify className={"size-4"} /> Connect
+                </Button>
+            </div>
+        )
+
+    if (!clientSDK)
+        return (
+            <div className={"text-xs text-muted-foreground"}>
+                SpotifyWebSDK is missing. <br /> Something went wrong.
+            </div>
+        )
+
+    return <SpotifyWebSDKContext.Provider value={clientSDK}>{children}</SpotifyWebSDKContext.Provider>
+}
+
+export default SpotifyWebSDKProvider
