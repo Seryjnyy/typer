5019017f9a0071b31c78b7b27162ccc57a93893a
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-12-12 15:20:21 +0000

    Add Spotify feature guard
    - Added a component that wraps any Spotify features to make sure user
    wants to use these features.
    
    - Not sure if it will be needed for the web player, but it should be
    used
     in create song form.

---
+++ src/components/spotify-new/spotify-feature-guard.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button.tsx"
+import { atomWithStorage } from "jotai/utils"
+import { useAtom } from "jotai/index"
+import { ReactNode } from "react"
+import { Icons } from "@/components/icons.tsx"
+
+// TODO : move this out
+const isUseSpotify = atomWithStorage("typer:is-use-spotify", false)
+const useSpotifySetting = () => useAtom(isUseSpotify)
+
+// TODO :I'm not sure if this component is needed anymore, I added this because Spotify did auto redirect because it was set up to
+// authenticate
+// in useEffect. But its manual now so idk.
+function SpotifyFeatureGuard({ children }: { children: ReactNode }) {
+    const [isWantToUseSpotify, setIsWantToUseSpotify] = useSpotifySetting()
+
+    if (!isWantToUseSpotify)
+        return (
+            <div>
+                <Button onClick={() => setIsWantToUseSpotify(true)} variant={"secondary"} className={"flex items-center gap-2"}>
+                    <Icons.spotify className={"size-4"} /> Turn on Spotify
+                </Button>
+            </div>
+        )
+
+    return children
+}
+
+export default SpotifyFeatureGuard
