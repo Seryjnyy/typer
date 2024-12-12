ea2ab81a4711b123bc831b3dae41f5a2fa9f26d4
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-12-12 19:37:02 +0000

    Create updated floating Spotify player

---
+++ src/components/spotify-new/player/floating-spotify-player.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/utils.ts"
+import SpotifyFeatureGuard from "@/components/spotify-new/spotify-feature-guard.tsx"
+import SpotifyPlayer from "@/components/spotify-new/player/spotify-player.tsx"
+import { GripVertical } from "lucide-react"
+import { Button } from "@/components/ui/button.tsx"
+
+// TODO : React rnd
+export default function FloatingSpotifyPlayer() {
+    return (
+        <div
+            className={cn(
+                "fixed bottom-[4rem] right-0 backdrop-blur-md z-50 p-3  m-3 outline-1 outline rounded-lg max-w-screen-md flex overflow-hidden" +
+                    " items-center"
+            )}
+        >
+            <SpotifyFeatureGuard>
+                <SpotifyPlayer />
+            </SpotifyFeatureGuard>
+            <div>
+                <Button variant={"outline"} size={"icon"} className={"ml-3"}>
+                    <GripVertical />
+                </Button>
+            </div>
+        </div>
+    )
+}
