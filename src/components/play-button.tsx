0f04bed9f616b429d55825c3e302c5566eb35b93
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-12-13 19:42:47 +0000

    Make PlayButton component more extensible
    - Added shadcn/ui button props to it so it can get all of that stuff.
    
    - Added children prop.
    
    - Added redirect prop so user can choose the behaviour.

--- src/components/play-button.tsx
+++ src/components/play-button.tsx
@@ -1,28 +1,44 @@
 import React from "react"
-import { Button } from "./ui/button"
+import { Button, ButtonProps } from "./ui/button"
 import { PlayIcon } from "@radix-ui/react-icons"
 import { useQueueStore } from "@/lib/store/queue-store"
 import MusicPlaying from "./music-playing"
 import usePlaySong from "@/lib/hooks/use-play-song"
+import { cn } from "@/lib/utils.ts"
 
-export default function PlayButton({ songID }: { songID: string }) {
+export default function PlayButton({
+    songID,
+    variant = "ghost",
+    size = "icon",
+    children,
+    onClick,
+    className,
+    redirect,
+    ...props
+}: { songID: string; redirect?: boolean } & ButtonProps) {
     const playSong = usePlaySong()
     const current = useQueueStore.use.current()
 
     const onPlay = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.stopPropagation()
-        playSong(songID)
+
+        playSong(songID, redirect)
     }
 
     return (
         <Button
-            onClick={(e) => onPlay(e)}
-            size={"icon"}
-            className="rounded-full hover:bg-background hover:text-foreground"
-            variant={"ghost"}
+            onClick={(e) => {
+                onPlay(e)
+                onClick?.(e)
+            }}
+            size={size}
+            className={cn("rounded-full hover:bg-background hover:text-foreground flex gap-2 items-center", className)}
+            variant={variant}
+            {...props}
         >
             {current == songID && <MusicPlaying />}
             {current != songID && <PlayIcon className="w-5 h-5" />}
+            {children}
         </Button>
     )
 }
