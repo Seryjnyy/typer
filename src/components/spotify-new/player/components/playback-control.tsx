35274a54b7cb2a542b3679cf8d7799fbdbfb69ab
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-12-13 20:50:02 +0000

    Add loading to playback-control
    - Added a loader that shows when playback is loading. Also disabled the
    button if loading.
    
    - Also added tooltip.

--- src/components/spotify-new/player/components/playback-control.tsx
+++ src/components/spotify-new/player/components/playback-control.tsx
@@ -1,15 +1,36 @@
 import { Button } from "@/components/ui/button.tsx"
-import { Pause, Play } from "lucide-react"
+import { Loader2, Pause, Play } from "lucide-react"
 import { usePlaybackState, useSpotifyPlayer } from "react-spotify-web-playback-sdk"
+import { TooltipProvider, Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip.tsx"
 
 export default function PlaybackControl() {
     const player = useSpotifyPlayer()
     const playbackState = usePlaybackState()
 
     return (
-        <Button onClick={() => player?.togglePlay()} size={"icon"} variant={"secondary"}>
-            {!playbackState?.paused && <Pause />}
-            {playbackState?.paused && <Play />}
-        </Button>
+        <TooltipProvider>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <Button
+                        onClick={() => player?.togglePlay()}
+                        size={"icon"}
+                        variant={"secondary"}
+                        className={"relative"}
+                        disabled={playbackState?.loading}
+                    >
+                        {!playbackState?.paused && <Pause />}
+                        {playbackState?.paused && <Play />}
+                        {(playbackState?.loading || !playbackState) && (
+                            <div className={"absolute backdrop-blur-sm w-full h-full flex items-center justify-center rounded-lg"}>
+                                <Loader2 className={"animate-spin"} />
+                            </div>
+                        )}
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>{playbackState?.paused ? "Play" : "Pause"}</p>
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
     )
 }
