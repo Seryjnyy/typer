e85e7a13aabffea8faefb14fe7d893a1bbba66b5
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-12-14 17:27:52 +0000

    Add shortcut info to playback-seek-bar

--- src/components/spotify-new/player/components/playback-seek-bar.tsx
+++ src/components/spotify-new/player/components/playback-seek-bar.tsx
@@ -1,7 +1,9 @@
 import { Slider } from "@/components/ui/slider.tsx"
-import { formatDurationMS } from "@/lib/utils.ts"
-import { useState } from "react"
+import { cn, formatDurationMS } from "@/lib/utils.ts"
+import { useEffect, useState } from "react"
 import { usePlaybackState, useSpotifyPlayer } from "react-spotify-web-playback-sdk"
+import { ArrowLeft, ArrowRight } from "lucide-react"
+import { SPOTIFY_WEB_PLAYER_SHORTCUTS, useShortcutInfo } from "@/lib/store/shortcuts-store.ts"
 
 export default function PlaybackSeekBar() {
     const spotifyPlayer = useSpotifyPlayer()
@@ -18,24 +20,59 @@ export default function PlaybackSeekBar() {
     }
 
     const handleChangeCommit = (val: number) => {
-        // set seeking position
         setSeeking(false)
         spotifyPlayer?.seek(val)
     }
 
     return (
-        <div className="flex gap-2">
-            <div className="text-xs text-muted-foreground pr-2">{formatDurationMS(position)}</div>
+        <div className={"relative"}>
+            <div className="flex gap-2">
+                <div className="text-xs text-muted-foreground pr-2">{formatDurationMS(position)}</div>
 
-            <Slider
-                disabled={playbackState === null}
-                value={isSeeking ? [seekingPosition] : [position]}
-                max={duration}
-                step={1}
-                onValueChange={(val) => handleChange(val[0])}
-                onValueCommit={(val) => handleChangeCommit(val[0])}
-            />
-            <div className="text-xs text-muted-foreground pl-2">{formatDurationMS(duration)}</div>
+                <Slider
+                    disabled={playbackState === null}
+                    value={isSeeking ? [seekingPosition] : [position]}
+                    max={duration}
+                    step={1}
+                    onValueChange={(val) => handleChange(val[0])}
+                    onValueCommit={(val) => handleChangeCommit(val[0])}
+                />
+                <div className="text-xs text-muted-foreground pl-2">{formatDurationMS(duration)}</div>
+            </div>
+            <div className={"text-xs text-muted-foreground w-full flex items-center justify-between px-12 absolute -top-5"}>
+                <PlaybackSeekShortcutsInfo />
+            </div>
         </div>
+    )
+}
+
+const PlaybackSeekShortcutsInfo = () => {
+    const [hide, setHide] = useState(false)
+    const seekForwardShortcut = useShortcutInfo(SPOTIFY_WEB_PLAYER_SHORTCUTS.PLAYBACK_SEEK_FORWARD)
+    const seekBackwardShortcut = useShortcutInfo(SPOTIFY_WEB_PLAYER_SHORTCUTS.PLAYBACK_SEEK_BACKWARD)
+
+    // Hide the shortcut info after a certain amount of time, since they clog up the UI
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setHide(true)
+        }, 10000)
+
+        return () => clearTimeout(timeout)
+    }, [])
+
+    return (
+        <>
+            {seekBackwardShortcut && seekBackwardShortcut.enabled && (
+                <span className={cn({ "opacity-0 transition-opacity duration-500": hide })}>
+                    <ArrowLeft className={"size-3 inline"} />
+                    (alt + j)
+                </span>
+            )}
+            {seekForwardShortcut && seekForwardShortcut.enabled && (
+                <span className={cn({ "opacity-0 transition-opacity duration-500": hide })}>
+                    (alt + l) <ArrowRight className={"size-3 inline"} />
+                </span>
+            )}
+        </>
     )
 }
