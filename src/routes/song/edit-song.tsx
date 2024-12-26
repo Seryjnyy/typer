29bbba38379540a9869803d12a56332cd78ed305
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-12-26 23:06:10 +0000

    Update edit/create song to use the new form
    - Updated edit-song.tsx to use new form.
    
    - Updated songs.tsx to use new form.

--- src/routes/song/edit-song.tsx
+++ src/routes/song/edit-song.tsx
@@ -1,52 +1,45 @@
-import BackButton from "@/components/ui/back-button";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { useSongStore } from "@/lib/store/song-store";
-import { useNavigate, useParams } from "react-router-dom";
-import EditSongForm from "./edit-song-form";
+import BackButton from "@/components/ui/back-button"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { useSongStore } from "@/lib/store/song-store"
+import { useNavigate, useParams } from "react-router-dom"
+import SongForm from "@/components/song-form/song-form.tsx"
 
 // TODO : Duplicate code with song page, also very similar code to verse page
 const SomethingWentWrong = () => {
     return (
         <div className="flex justify-center items-center h-full flex-col gap-12">
             <div className="text-center">
-                <h1 className="text-xl font-semibold">
-                    Sorry something went wrong.
-                </h1>
+                <h1 className="text-xl font-semibold">Sorry something went wrong.</h1>
                 <p>Please go back and try again.</p>
             </div>
             <BackButton link="/songs" />
         </div>
-    );
-};
+    )
+}
 
 export default function EditSong() {
-    const songs = useSongStore.use.songs();
-    const { songID } = useParams();
-    const navigate = useNavigate();
+    const songs = useSongStore.use.songs()
+    const { songID } = useParams()
+    const navigate = useNavigate()
 
     if (!songID) {
-        throw Error("No song ID provided.");
+        throw Error("No song ID provided.")
     }
 
-    const song = songs.find((x) => x.id == songID);
+    const song = songs.find((x) => x.id == songID)
 
     // TODO : Could be better
-    if (!song) return <SomethingWentWrong />;
+    if (!song) return <SomethingWentWrong />
 
     return (
-        <div className="h-[100%] rounded-md overflow-hidden">
-            <ScrollArea className="h-[100%]  px-2 sm:px-6 md:px-12 sm:pb-2 pb-4 pt-12">
-                <div className="flex flex-col items-start justify-start">
+        <div className="h-[100%] rounded-md overflow-hidden ">
+            <ScrollArea className="h-[100%]  sm:pb-2 pb-4 pt-12 ">
+                <div className="flex flex-col items-start justify-start  px-2 sm:px-6 md:px-12">
                     <BackButton link="/songs" />
                     <h1 className="text-2xl font-bold py-4">Edit song</h1>
                 </div>
-                <div className="px-2">
-                    <EditSongForm
-                        song={song}
-                        onSuccess={() => navigate("/songs")}
-                    />
-                </div>
+                <SongForm initialValues={song} onSuccess={() => navigate("/songs")} />
             </ScrollArea>
         </div>
-    );
+    )
 }
--- src/routes/song/songs.tsx
+++ src/routes/song/songs.tsx
@@ -1,28 +1,22 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { PlusIcon } from "@radix-ui/react-icons";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { PlusIcon } from "@radix-ui/react-icons"
 
-import { useSearchParams } from "react-router-dom";
-import { useSongStore } from "../../lib/store/song-store";
-import CreateSongForm from "./create-song-form";
+import { useSearchParams } from "react-router-dom"
+import { useSongStore } from "../../lib/store/song-store"
 
-import Playlists from "./playlists";
-import SongsList from "./songs-list";
-import StatsPage from "./stats-page";
+import Playlists from "./playlists"
+import SongsList from "./songs-list"
+import SongForm from "@/components/song-form/song-form.tsx"
 
 export default function Songs() {
-    const [searchParams] = useSearchParams();
-    const songs = useSongStore.use.songs();
+    const [searchParams] = useSearchParams()
+    const songs = useSongStore.use.songs()
 
-    let tab = searchParams.get("tab");
+    let tab = searchParams.get("tab")
 
     if (tab) {
-        if (
-            tab != "all-songs" &&
-            tab != "add-song" &&
-            tab != "stats" &&
-            tab != "playlists"
-        ) {
-            tab = null;
+        if (tab != "all-songs" && tab != "add-song" && tab != "stats" && tab != "playlists") {
+            tab = null
         }
     }
 
@@ -31,36 +25,23 @@ export default function Songs() {
             <TabsList className="mx-2 sm:mx-6 md:mx-12">
                 <TabsTrigger value={"all-songs"}>Songs</TabsTrigger>
 
-                <TabsTrigger
-                    value="add-song"
-                    className="flex items-center gap-1"
-                >
+                <TabsTrigger value="add-song" className="flex items-center gap-1">
                     <PlusIcon />
                     Add song
                 </TabsTrigger>
                 <TabsTrigger value="playlists">Playlists</TabsTrigger>
-                <TabsTrigger value="stats">Stats</TabsTrigger>
             </TabsList>
-            <TabsContent
-                value="all-songs"
-                className=" h-[100%] px-2 sm:px-6 md:px-12"
-            >
+            <TabsContent value="all-songs" className=" h-[100%] px-2 sm:px-6 md:px-12">
                 <div className="h-full w-full relative">
                     <SongsList songsList={songs} />
                 </div>
             </TabsContent>
-            <TabsContent
-                value="playlists"
-                className=" h-[100%] px-2 sm:px-6 md:px-12"
-            >
+            <TabsContent value="playlists" className=" h-[100%] px-2 sm:px-6 md:px-12">
                 <Playlists />
             </TabsContent>
             <TabsContent value="add-song" className=" h-[100%] ">
-                <CreateSongForm />
-            </TabsContent>
-            <TabsContent value="stats" className=" h-[100%]  border-t">
-                <StatsPage />
+                <SongForm initialValues={undefined} />
             </TabsContent>
         </Tabs>
-    );
+    )
 }
